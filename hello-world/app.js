fe5220ed181441e5450e0ee2e8a636007adfff94
Enrique Palacios <enrique.palacios@struck.com>
2020-11-26 23:34:26 -0600

    Initial commit

---
+++ hello-world/app.js
@@ -0,0 +1,40 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
+let response;
+
+exports.lambdaHandler = async (event, context) => {
+  try {
+    await getUser(event.cognitoId).then((data) => {
+      // const user = data.Items[0];
+      console.log("the element is", event, data);
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({}),
+      };
+    });
+  } catch (err) {
+    console.log("the error is", err);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: err,
+      }),
+    };
+    return err;
+  }
+
+  return response;
+};
+
+function getUser(cognitoId) {
+  var params = {    
+    ExpressionAttributeValues: {
+      ":cognitoId": { S: cognitoId },
+    },
+    ProjectionExpression: 'cognitoId, email, subscription, createdAt, id',
+    KeyConditionExpression: "id <> null AND cognitoId = :cognitoId",
+    TableName: process.env.TABLE_NAME
+  };
+
+  return docClient.query(params).promise();
+}
